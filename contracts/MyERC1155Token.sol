92f7fe3e6862d32890a2ec2f65033592330a19f7
Pedro Magalhães <pedro@scrumlaunch.com>
2023-04-19 13:56:10 -0300

    tutorial finished

---
+++ contracts/MyERC1155Token.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+
+contract MyERC1155Token is ERC1155 {
+    constructor(string memory uri) ERC1155(uri) {
+    }
+
+    function mint(address to, uint256 id, uint256 amount, bytes memory data) public {
+        _mint(to, id, amount, data);
+    }
+
+    function mintBatch(address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data) public {
+        _mintBatch(to, ids, amounts, data);
+    }
+}
---
+++ test/MyERC1155Token.test.js
@@ -0,0 +1,59 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("MyERC1155Token", function () {
+  let MyERC1155Token, myERC1155Token, owner, addr1, addr2;
+
+  beforeEach(async function () {
+    MyERC1155Token = await ethers.getContractFactory("MyERC1155Token");
+    [owner, addr1, addr2] = await ethers.getSigners();
+    myERC1155Token = await MyERC1155Token.deploy("https://example.com/metadata/");
+  });
+
+  describe("Implantação", function () {
+    it("Deve definir o URI correto", async function () {
+      expect(await myERC1155Token.uri(0)).to.equal("https://example.com/metadata/");
+    });
+  });
+
+  describe("Cunhagem de tokens", function () {
+    it("Deve emitir tokens para o endereço especificado", async function () {
+      await myERC1155Token.mint(addr1.address, 1, 50, "0x");
+      expect(await myERC1155Token.balanceOf(addr1.address, 1)).to.equal(50);
+    });
+
+    it("Deve emitir em lote tokens para o endereço especificado", async function () {
+      const ids = [1, 2, 3];
+      const amounts = [50, 30, 10];
+      await myERC1155Token.mintBatch(addr1.address, ids, amounts, "0x");
+
+      expect(await myERC1155Token.balanceOf(addr1.address, 1)).to.equal(50);
+      expect(await myERC1155Token.balanceOf(addr1.address, 2)).to.equal(30);
+      expect(await myERC1155Token.balanceOf(addr1.address, 3)).to.equal(10);
+    });
+  });
+
+  describe("Transferência de tokens", function () {
+    beforeEach(async function () {
+      await myERC1155Token.mint(addr1.address, 1, 50, "0x");
+      await myERC1155Token.mint(addr1.address, 2, 30, "0x");
+    });
+
+    it("Deve transferir tokens entre endereços", async function () {
+      await myERC1155Token.connect(addr1).safeTransferFrom(addr1.address, addr2.address, 1, 20, "0x");
+      expect(await myERC1155Token.balanceOf(addr1.address, 1)).to.equal(30);
+      expect(await myERC1155Token.balanceOf(addr2.address, 1)).to.equal(20);
+    });
+
+    it("Deve transferir em lote tokens entre endereços", async function () {
+      const ids = [1, 2];
+      const amounts = [10, 5];
+      await myERC1155Token.connect(addr1).safeBatchTransferFrom(addr1.address, addr2.address, ids, amounts, "0x");
+
+      expect(await myERC1155Token.balanceOf(addr1.address, 1)).to.equal(40);
+      expect(await myERC1155Token.balanceOf(addr1.address, 2)).to.equal(25);
+      expect(await myERC1155Token.balanceOf(addr2.address, 1)).to.equal(10);
+      expect(await myERC1155Token.balanceOf(addr2.address, 2)).to.equal(5);
+    });
+  });
+});
